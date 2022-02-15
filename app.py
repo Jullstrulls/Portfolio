853b98fe5eb688271aa24e0bc708284a8a0ff906
julia johansson <johansson99julia@gmail.com>
2022-02-15 13:29:42 +0100

    api for loading json file with course information. Send data to html file and implemented generic html markup

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from api import *
 
 app = Flask("Portfolio")
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    courses = load("courses.json")
+    print(courses)
+    return render_template("index.html", courses=courses)
 
 #debug live with flask
 if __name__ == "__main__":
