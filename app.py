c231b83785303e5452da83c70af17fb42b17f353
julia johansson <johansson99julia@gmail.com>
2022-02-15 14:35:02 +0100

    started portfolio project listing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from api import *
 
 app = Flask("Portfolio")
@@ -6,8 +6,18 @@ app = Flask("Portfolio")
 @app.route('/')
 def index():
     courses = load("courses.json")
-    print(courses)
-    return render_template("index.html", courses=courses)
+    projects = load("projects.json")
+    return render_template("index.html", courses=courses, projects=projects)
+
+@app.route("/project/<int:id>")
+def info(id):
+    data = load("data.json")
+    #info_project = get_project(data, id)                               #Use function from api to get project dict from id
+    #if info_project == None:                                           #If there is no project that matches id, abort to errorhandler 404
+     #   abort(404)
+    #else:                                                             
+     #   return render_template("info.html", data = info_project, id = id)
+
 
 #debug live with flask
 if __name__ == "__main__":
